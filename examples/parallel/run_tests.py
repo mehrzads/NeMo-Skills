ca7f3817025e1cab5dbffe044dde3c3bb1fac2d8
msamadi <msamadi@nvidia.com>
2025-09-16 14:38:41 -0700

    test

--- examples/parallel/run_tests.py
+++ examples/parallel/run_tests.py
@@ -94,21 +94,23 @@ def main( code_input_files: List[str],  cluster: str, ref_file: str, test_file: 
             f"    --test_file={test_file} "       
         )
         
-        
-
-        run_cmd(
-            ctx=wrap_arguments(""), # No hydra overrides needed for this simple script dispatch
-            cluster=cluster,
-            command=eval_command,
-            expname=eval_job_expname,
-            log_dir=str(eval_log_dir),
-            num_nodes=server_nodes,
-            num_gpus=server_gpus,
-            with_sandbox=True,
-            get_random_port=True,
-            #exclusive=True,
-            time_min=merge_time_min,
-        )
+        for i in range(4):
+            eval_job_expname = f"{base_code_expname}_eval_run_{i}"
+            prev_eval_job_expname = f"{base_code_expname}_eval_run_{i-1}"
+            run_cmd(
+                ctx=wrap_arguments(""), # No hydra overrides needed for this simple script dispatch
+                cluster=cluster,
+                command=eval_command,
+                expname=eval_job_expname,
+                log_dir=str(eval_log_dir),
+                num_nodes=server_nodes,
+                num_gpus=server_gpus,
+                with_sandbox=True,
+                get_random_port=True,
+                run_after=[prev_eval_job_expname],
+                #exclusive=True,
+                time_min=merge_time_min,
+            )
    
     print(f"--- Submitted Evaluation Job for Run --- (Cluster: {cluster})")
 
