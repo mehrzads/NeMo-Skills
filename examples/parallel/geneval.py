a7fe586eed21b79327e5d6ad79eb556be79d32fe
msamadi <msamadi@nvidia.com>
2025-10-23 12:36:04 -0700

    test

--- examples/parallel/geneval.py
+++ examples/parallel/geneval.py
@@ -1,7 +1,7 @@
 from nemo_skills.pipeline.eval import eval
 from nemo_skills.pipeline.cli import generate, run_cmd, wrap_arguments
 
-benchmark = "icpc25"
+benchmark = "ioi25"
 if benchmark == "ioi25":
 
 
@@ -21,12 +21,12 @@ if benchmark == "ioi25":
             model=f"/hf_models/gpt-oss-120b",
             server_type='vllm',
             server_gpus=8,
-            num_jobs=1,
-            benchmarks="ioi25:1",
+            num_jobs=10,
+            benchmarks="ioi25:50",
             data_dir="/workspace/llmcoding/eval_dataset/",
             output_dir="/workspace/test/",
-            split="test",
-            time_min="00:30:00",
+            split="/workspace/eval/ioi25_50",
+            time_min="04:00:00",
             extra_eval_args=f"++eval_config.test_file=/workspace/llmcoding/eval_dataset/ioi25/test_metadata.json",
     )
 elif benchmark == "icpc25":
