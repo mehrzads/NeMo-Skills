66b2282dede806665d62c6090d2dbcbc8645b1d6
msamadi <msamadi@nvidia.com>
2025-10-25 14:57:01 -0700

    test

--- examples/parallel/geneval.py
+++ examples/parallel/geneval.py
@@ -1,7 +1,7 @@
 from nemo_skills.pipeline.eval import eval
 from nemo_skills.pipeline.cli import generate, run_cmd, wrap_arguments
 
-benchmark = "icpc25"
+benchmark = "ioi25_diversity"
 if benchmark == "ioi25":
 
 
@@ -29,6 +29,34 @@ if benchmark == "ioi25":
             time_min="04:00:00",
             extra_eval_args=f"++eval_config.test_file=/workspace/llmcoding/eval_dataset/ioi25/test_metadata.json",
     )
+elif benchmark == "ioi25_diversity":
+
+
+    eval(
+            ctx=wrap_arguments(
+                "++skip_filled=True "
+                "++prompt_config=/workspace/prompts/diversity.yaml"
+                "++inference.temperature=1.0 "
+                "++inference.top_p=1.0 "
+                "++inference.tokens_to_generate=120000 " 
+                "++inference.extra_body.reasoning_effort=high "
+                "++max_concurrent_requests=1024 "
+            ),
+            cluster="oci-ord-mz",
+            with_sandbox=True,
+            expname=f"ioi25_diversity_eval_run",
+            model=f"/hf_models/gpt-oss-120b",
+            server_type='vllm',
+            server_gpus=8,
+            num_jobs=10,
+            benchmarks="ioi25:50",
+            data_dir="/workspace/llmcoding/eval_dataset/",
+            output_dir="/workspace/eval/ioi25_50_diversity",
+            split="test",
+            time_min="04:00:00",
+            extra_eval_args=f"++eval_config.test_file=/workspace/llmcoding/eval_dataset/ioi25/test_metadata.json",
+    )
+
 elif benchmark == "icpc25":
     eval(
             ctx=wrap_arguments(
