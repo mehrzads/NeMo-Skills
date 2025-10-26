8d3b405a977a49a5f46987778fd2920487933963
msamadi <msamadi@nvidia.com>
2025-10-25 18:14:29 -0700

    metics

--- nemo_skills/evaluation/metrics/icpc_metrics.py
+++ nemo_skills/evaluation/metrics/icpc_metrics.py
@@ -58,7 +58,7 @@ class ICPCMetrics(BaseMetrics):
                 self.problem_scores[name] = True
             self.total_submissions[name] += 1    
         self.print_problem_scores()
-        metrics_dict = {}
+        metrics_dict = {"icpc25": {}}
         for name, scores in self.problem_scores.items():
             metrics_dict["icpc25"][name] = {
                 "correct": self.correct_submissions[name],
