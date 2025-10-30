c8608094171580df0bfeead40eeed9e0f0c49c37
msamadi <msamadi@nvidia.com>
2025-10-30 13:06:27 -0700

    gencluster

--- recipes/gencluster/pipeline/test_case_generations.py
+++ recipes/gencluster/pipeline/test_case_generations.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
         with_sandbox=True,
     )
 
-    genrate_tests_command = f"python /nemo_run/code/recipes/gencluster/scripts/extract_cpp_code.py --input_dir {output_dir} ; python /nemo_run/code/recipes/gencluster/scripts/generate_tests.py 1 --min-validators 1 --base_dir {output_dir}"
+    genrate_tests_command = f"python /nemo_run/code/recipes/gencluster/scripts/extract_cpp_code.py --input_dir {output_dir} ; python /nemo_run/code/recipes/gencluster/scripts/generate_tests.py 1 --min-validators 1 --base-dir {output_dir}"
     run_cmd(
         ctx=wrap_arguments(""), 
         cluster=cluster,
