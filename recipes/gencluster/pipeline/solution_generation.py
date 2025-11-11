86bf10ce35f7cb22c5d644f03422920fec7b90da
msamadi <msamadi@nvidia.com>
2025-11-11 11:20:28 -0800

    test

--- recipes/gencluster/pipeline/solution_generation.py
+++ recipes/gencluster/pipeline/solution_generation.py
@@ -15,7 +15,9 @@ eval(
         "++inference.tokens_to_generate=120000 "
         "++inference.extra_body.reasoning_effort=high "
         "++max_concurrent_requests=1024 "
-        "++eval_config.test_file=/workspace/llmcoding/eval_dataset/icpc/icpc24_metadata.json "
+        "++eval_config.test_file=/workspace/llmcoding/eval_dataset/icpc/icpc25_metadata.json "
+        "++eval_config.input_file=/workspace/results/icpc25/gpt-oss-120b/gencluster/gpt-oss-120b_100.json "
+
     ),
     cluster="iad",
     with_sandbox=True,
@@ -23,12 +25,11 @@ eval(
     model="/hf_models/gpt-oss-120b",
     server_type="vllm",
     server_gpus=8,
-    num_jobs=10,
-    benchmarks="icpc:50",
+    num_jobs=100,
+    benchmarks="icpc:1000",
     data_dir="/workspace/llmcoding/eval_dataset/",
     output_dir=f"/workspace/results/icpc24/gpt-oss-120b/generation/run_{run_number}/",
     metrics_kwargs=f'{{"cluster_folder": "/workspace/results/icpc24/gpt-oss-120b/generation/run_{run_number}/clusters/"}}',
-    split="icpc24",
+    split="icpc25",
     time_min="04:00:00",
 )
-#        "++eval_config.input_file=/workspace/results/icpc25/gpt-oss-120b/gencluster/gpt-oss-120b_100.json "
